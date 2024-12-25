6e8aba7271688b841f67d372226ef11dbac61e61
SunMendi <mehedimahbub706@gmail.com>
2024-12-25 18:41:03 +0600

    server deployment

--- voice/settings.py
+++ voice/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,10 +84,18 @@ WSGI_APPLICATION = 'voice.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'railway',
+         'USER': 'postgres',
+         'PASSWORD': os.environ['DB_PASSWORD'],
+         'HOST': 'autorack.proxy.rlwy.net',
+         'PORT': '19662',
+         }
 }
 
 
